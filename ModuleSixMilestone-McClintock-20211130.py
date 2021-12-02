af23ec82b3de4e08becab7354fd92cd42be71af7
jmmccli <86330067+jmmccli@users.noreply.github.com>
2021-12-02 12:37:31 -0500

    Update ModuleSixMilestone-McClintock-20211130.py

--- ModuleSixMilestone-McClintock-20211130.py
+++ ModuleSixMilestone-McClintock-20211130.py
@@ -1,6 +1,5 @@
 # Lord Jeremy M. McClintock, CISSP
 
-global curr_room  # Define a global variable to keep track of the current room the player is in.
 # A dictionary for the Trogdor the Burninator text game
 # The dictionary links a room to other rooms.
 rooms = {
@@ -17,7 +16,7 @@ rooms = {
 
 def print_curr_room():
     room = rooms.get(curr_room)  # Get the current room from the Dictionary
-    if (AttributeError == 'NoneType'):  # Error handling
+    if AttributeError == 'NoneType':  # Error handling
         print(AttributeError.name)
     else:
         print('You are in the ' + curr_room + '.', end=' ')  # Print current room
@@ -25,20 +24,20 @@ def print_curr_room():
             print('You can go ' + str(direction) + " to the " + str(potential_room) + '.')
 
 
-def get_curr_room_info():
-    return curr_room
-
-
 def move_room(direction):
-    current_room = get_curr_room_info()  # Get current room's info
-    room = rooms.get(current_room)  # Define room list
+    current_room = curr_room  # Get current room's info
+    room = rooms.get(curr_room)  # Define room list
     for direct, potential_room in room.items():  # Iterate over the list, looking for direction & potential room
-        if (direct.lower() == direction.lower()):  # Check to see if direction matches list
+        if direct.lower() == direction.lower():  # Check to see if direction matches list
             current_room = potential_room  # Set current room to potential room
-        elif (potential_room.lower() == direction.lower()):  # Check to see if potential room matches list
+            break
+        elif potential_room.lower() == direction.lower():  # Check to see if potential room matches list
             current_room = potential_room  # Set current room to potential room
+            break
         else:
             continue  # If not a match, continue to the next iteration in the list
+    else:
+        print('Invalid Entry. Enter the direction you would like to go.')
 
     return current_room
 
@@ -55,6 +54,7 @@ room_items = {
 }
 
 
+# FIXME #Get dictionary Functionality Fixed (Current Priority)
 def print_room_items():
     room_item = room_items.get(curr_room)
     for person, item in room_item.items():
@@ -64,6 +64,7 @@ def print_room_items():
             continue  # If not a match, continue to the next iteration in the list
 
 
+# Small dictionary for inventory items
 inventory = {
     'Thatched Roof Cottages': 'Mot Burninated',
     'Peasants': 'Not Burninated',
@@ -73,11 +74,13 @@ inventory = {
 }
 
 
+# Print the inventory items
 def print_inventory():
-    for item, has_item in inventory.items():
-        print(str(item) + ': ' + str(has_item))
+    for item, has_item in inventory.items():  # Iterate over each key in the dictionary
+        print(str(item) + ': ' + str(has_item))  # Print each key, item pair
 
 
+# FIXME #Once room_items is up & running, verify functionality and adjust as necessary
 def get_item(item):
     for inv_item, has_item in inventory.items():
         if(inv_item.lower() == item):
@@ -92,6 +95,8 @@ def get_item(item):
 
 
 def main():
+    global curr_room  # Define a global variable to keep track of the current room the player is in.
+
     curr_room = 'Bedroom'  # Initialize the variable curr_room
     pot_room = 'Bedroom'  # Initialize the variable for potential rooms the player can move to based on current room.
 
@@ -103,7 +108,7 @@ def main():
         else:
 
             print_curr_room()  # Print the current room that the player is in as well as the rooms they can move to.
-            print_room_items()
+            #print_room_items() #FIXME
 
             pot_room = input('Type Exit to quit. Type Inventory to view Inventory.' + '\n')
             # Get input from user (which room they want to move to).
